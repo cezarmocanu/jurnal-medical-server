f8972950c16dd9fc2e651d07a34c62b2e46e2790
fabian1999 <fabian.bertea@yahoo.com>
2020-12-02 20:16:48 +0200

    [BE0031] Created Author Database

--- src/db/index.js
+++ src/db/index.js
@@ -3,14 +3,20 @@ const connection = require('./connection');
 
 const createArticleModel = require('./article.db');
 const createEditionModel = require('./edition.db');
+const createAuthorModel = require('./author.db');
 
 createArticleModel(connection, DataTypes);
 createEditionModel(connection, DataTypes);
+createAuthorModel(connection, DataTypes);
 
 
 connection.models.edition.hasMany(connection.models.article);
 connection.models.article.belongsTo(connection.models.edition,{
     foreignKey: "editionId"
+});
+connection.models.author.hasMany(connection.models.article);
+connection.models.article.belongsTo(connection.models.author,{
+    foreignKey: "authorId"
 });
 
 module.exports = {
