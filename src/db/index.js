9f03066aaf2e489920554319920c478ea63be0a3
TeodorBirghisan <48990501+TeodorBirghisan@users.noreply.github.com>
2020-12-02 14:34:48 +0200

    [BE0024]-create-an-edition-database-model

--- src/db/index.js
+++ src/db/index.js
@@ -2,9 +2,16 @@ const {DataTypes} = require('sequelize');
 const connection = require('./connection');
 
 const createArticleModel = require('./article.db');
+const createEditionModel = require('./edition.db');
 
 createArticleModel(connection, DataTypes);
+createEditionModel(connection, DataTypes);
 
+
+connection.models.edition.hasMany(connection.models.article);
+connection.models.article.belongsTo(connection.models.edition,{
+    foreignKey: "editionId"
+});
 
 module.exports = {
     connection, 
