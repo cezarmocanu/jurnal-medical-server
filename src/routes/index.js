bf32ee1020c8dbdbbb3634a27e6a6afc5a2d2395
fabian1999 <fabian.bertea@yahoo.com>
2020-12-08 00:26:11 +0200

    [BE0036] Add Keyword route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 const articleRouter = require('./article.route');
 const editionRouter = require('./edition.route');
 const authorRouter = require('./author.route');
+const keywordRouter = require('./keyword.route');
 
 module.exports = (app) => {
     app.use('/article', articleRouter);
     app.use('/edition', editionRouter);
     app.use('/author', authorRouter);
+    app.use('/keyword', keywordRouter);
 }
