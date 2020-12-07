4adbcf3e6a888f9c75ec876db032c3a1abf289e5
TeodorBirghisan <48990501+TeodorBirghisan@users.noreply.github.com>
2020-12-07 21:31:29 +0200

    BE0034-add-collection-route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
-const articleRouter = require('./article.route');
-const editionRouter = require('./edition.route');
-const authorRouter = require('./author.route');
+const articleRouter = require("./article.route");
+const editionRouter = require("./edition.route");
+const authorRouter = require("./author.route");
+const collectionRouter = require("./collection.route");
 
 module.exports = (app) => {
-    app.use('/article', articleRouter);
-    app.use('/edition', editionRouter);
-    app.use('/author', authorRouter);
-}
+  app.use("/article", articleRouter);
+  app.use("/edition", editionRouter);
+  app.use("/author", authorRouter);
+  app.use("/collection", collectionRouter);
+};
