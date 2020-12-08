d4759e61ccf68c5050da648371b214f6d4197949
fabian1999 <fabian.bertea@yahoo.com>
2020-12-08 17:36:34 +0200

    [BE0059] Add update method to withCrud controller

--- src/constants.js
+++ src/constants.js
@@ -2,7 +2,8 @@
 const OP_PATHS = {
     GET_ONE_BY_ID: '/one/:id',
     GET_ALL: '/all',
-    CREATE_ONE: '/create/one'
+    CREATE_ONE: '/create/one',
+    UPDATE: '/update/:id'
 };
 
 module.exports = {
