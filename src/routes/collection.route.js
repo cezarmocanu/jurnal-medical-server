b8f47175a6b8b5c66b8f92f0283c88099afd49e1
TeodorBirghisan <48990501+TeodorBirghisan@users.noreply.github.com>
2020-12-08 10:34:41 +0200

    BE0034-add-collection-route

--- src/routes/collection.route.js
+++ src/routes/collection.route.js
@@ -2,6 +2,6 @@ const router = require("express").Router();
 const { models } = require("../db");
 const { withCrud } = require("../controllers/crud.controller");
 
-withCrud(router, models.edition);
+withCrud(router, models.collection);
 
 module.exports = router;
