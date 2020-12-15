727230fd7ea189e990e45934b8693dea119917a5
fabian1999 <fabian.bertea@yahoo.com>
2020-12-15 19:21:56 +0200

    [BE0091] Add a breadcrumb route

--- src/routes/breadcrumb.route.js
+++ src/routes/breadcrumb.route.js
@@ -1,6 +1,90 @@
 const router = require('express').Router();
-const {breadcrumbController} = require('../controllers/breadcrumb.controller');
+const {response} = require('../controllers/utils.controller');
+const _ = require('lodash');
+const {models} = require('../db');
+const {BREADCRUMB_PATHS} = require('../constants');
+const collection = require('../model/collection.model');
+const {COLLECTION, EDITION, ARTICLE} = BREADCRUMB_PATHS;
 
-breadcrumbController(router);
+router.get(COLLECTION, async (req,res) => {
+    try {
+
+        const {id} = _.get(req, 'params', null);
+
+        if (_.isNaN(id)){
+            return response(res).badRequest();
+        }
+
+        const entity = await models.collection.findByPk(id);
+
+        if (_.isNil(entity)) {
+            return response(res).notFound();
+        }
+
+        return response(res).ok(entity);
+    
+    } catch (error) {
+        return response(res).internalServerError({error})
+    }
+});
+
+router.get(EDITION, async (req,res)=> {
+    try {
+
+        const {id} = _.get(req, 'params', null);
+
+        if (_.isNaN(id)){
+            return response(res).badRequest();
+        }
+
+        const entity = await models.edition.findOne({
+            where:{id},
+            include: models.collection
+        });
+
+        if (_.isNil(entity)) {
+            return response(res).notFound();
+        }
+
+        const finalEntity = [{id: entity.collection.id, title: entity.collection.title}, {id: entity.id, title: entity.title, collectionId: entity.collectionId}];
+
+        return response(res).ok(finalEntity);
+
+    } catch (error) {
+        return response(res).internalServerError({error})
+    }
+});
+
+router.get(ARTICLE, async (req,res)=> {
+    try {
+
+        const {id} = _.get(req, 'params', null);
+
+        if (_.isNaN(id)){
+            return response(res).badRequest();
+        }
+
+        const entityArticle = await models.article.findOne({
+            where:{id},
+            include: models.edition
+        });
+
+        const entityEdition = await models.edition.findOne({
+            where: id == entityArticle.editionId,
+            include: models.collection
+        });
+
+        if (_.isNil(entityArticle)) {
+            return response(res).notFound();
+        }
+
+       const finalEntity = [{id: entityEdition.collection.id, title: entityEdition.collection.title}, {id: entityEdition.id, title: entityEdition.title, collectionId: entityEdition.collectionId}, {id: entityArticle.id, title: entityArticle.title, editionId: entityArticle.editionId}];
+
+        return response(res).ok(finalEntity);
+
+    } catch (error) {
+        return response(res).internalServerError({error})
+    }
+});
 
 module.exports = router;
