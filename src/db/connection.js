a01e65c1ad348287a630766d1f2cf9c0f683a0c2
Cezar Mocanu <cmocanu@Cezars-MacBook-Pro.local>
2020-12-18 12:21:56 +0200

    [production] Reset sync true

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,20 +1,9 @@
 const {Sequelize} = require('sequelize');
-const {database, username, password} = require('../config.dev');
+const {database, username, password, host} = require('../config.dev');
 
 
-const createConnection = () => {
-    if (process.env.HEROKU_POSTGRESQL_BRONZE_URL) {
-        new Sequelize(process.env.HEROKU_POSTGRESQL_BRONZE_URL, {
-            dialect: 'postgres',
-            protocol: "postgres",   
-            port: 5432,
-            logging: true
-        });
-    }
-
-    return new Sequelize(database, username, password, {
-        host: 'localhost',
+module.exports = new Sequelize(database, username, password, {
+        host: host || 'localhost',
         dialect: 'postgres'
     });
 }
-module.exports = createConnection();
